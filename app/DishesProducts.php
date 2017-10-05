cb12ac34ff85a714bbed80cb2fb3a0a0aca91b82
Alheor <mrAlheor@gmail.com>
2017-10-05 21:54:04 +0300

    База данных, миграции и модели сущностей

---
+++ app/DishesProducts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DishesProducts extends Model
+{
+    protected $table = 'dishes_products';
+    protected $primaryKey = 'id';
+}
---
+++ app/ProductCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    protected $table = 'products_categories';
+    protected $primaryKey = 'id';
+}
